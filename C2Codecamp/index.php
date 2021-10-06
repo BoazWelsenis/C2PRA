a6c4d9f6967cb42ad74541b22bf42420753933f8
Tijn Van de Velde <vandeveldetijn@gmail.com>
2021-10-06 14:33:05 +0200

    html/php nieuw ingevoegd

--- C2Codecamp/index.php
+++ C2Codecamp/index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="css/style.css" rel="stylesheet">
+    <title>Home</title>
 </head>
-<body>
-    
+<body>    
+    <header>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Boeken</a>
+            <a href="#">Films</a>
+            <a href="#">Muziek</a>
+            <a href="#">Gereedschap</a>
+            <div class="animation start-home"></div>
+        </nav>
+    </header>
+
+    <main>
+        <h1>Home</h1>
+    </main>
 </body>
 </html>
