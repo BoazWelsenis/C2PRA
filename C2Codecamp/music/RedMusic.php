16d5b996c4c3ec9254613d7ca7fc647deec972a0
MartijnBot <d280744@edu.rocwb.nl>
2021-10-11 12:40:33 +0200

    Songteksten toegevoegd

--- C2Codecamp/music/RedMusic.php
+++ C2Codecamp/music/RedMusic.php
@@ -11,8 +11,68 @@
     require_once('../components/header.php');
     ?>
 <body>
-    <h1>Plug in baby - Muse</h1>
-    <h1>Transylvia - Iron Maiden</h1>
-    <h1>Madness - Muse</h1>
+    <div class="wrapper">
+        <h1>Plug in baby - Muse</h1>
+        <p>I've exposed your lies, baby
+The underneath no big surprise
+Now it's time for changing
+And cleansing everything
+To forget your love
+My plug in baby
+Crucifies my enemies
+When I'm tired of giving
+My plug in baby
+In unbroken virgin realities
+Is tired of living
+Don't confuse
+Baby you're gonna lose
+Your own game
+Change me
+And replace the envying
+To forget your love
+My plug in baby
+Crucifies my enemies
+When I'm tired of giving
+My plug in baby
+In unbroken virgin realities
+And tired of living
+And I've seen your loving
+Mine is gone</p>
+        <h1>Transylvia - Iron Maiden</h1>
+        <p>Instrumental</p>
+        <h1>Madness - Muse</h1>
+        <p>I, I can't get these memories out of my mind
+And some kind of madness has started to evolve
+I, I tried so hard to let you go
+But some kind of madness is swallowing me whole, yeah
+I have finally seen the light
+And I have finally realized
+What you mean
+Oh oh oh
+And now, I need to know is this real love
+Or is it just madness keeping us afloat?
+And when I look back at all the crazy fights we had
+Like some kind of madness
+Was taking control
+Yeah
+And now I have finally seen the light
+And I have finally realized
+What you need
+Mm
+And now I have finally seen the end (Finally seen the end)
+And I'm not expecting you to care (Expecting you to care)
+But I have finally seen the light (Finally seen the light)
+I have finally realized (Realized)
+I need to love
+I need to love
+Come to me,
+Trust in your dream
+Come on and rescue me
+Yes I have known, I can be wrong
+Maybe I'm too headstrong
+Our love is
+Madness</p>
+    </div>
+
 </body>
 </html>
