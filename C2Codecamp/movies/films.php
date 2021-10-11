e833b204d280d8c47399a20e32d3f5316da17356
BoazWelsenis <D281029@edu.rocwb.nl>
2021-10-11 12:31:28 +0200

    Movies Folder + Pagina aangemaakt
    De Movies Folder is aangemaakt met daarin de films.php hierin staat de informatie dat de klant wilt zien over de films.

--- C2Codecamp/movies/films.php
+++ C2Codecamp/movies/films.php
@@ -4,26 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
     <title>C2 CodeCamp - Films</title>
 </head>
 <body>    
-    <?php include_once('header.php'); ?>
+    <?php include_once('../components/header.php'); ?>
 
     <main>
         <h1 class="title-page">Films</h1>
 
     <div class="img-movies">   
         <a href="https://www.imdb.com" target="_blank">
-        <img alt="imdb" src="img/imdb-logo.png" width="100" height="100">
+        <img alt="imdb" src="../img/imdb-logo.png" width="100" height="100">
         </a>
 
         <a href="https://www.videoland.nl" target="_blank">
-        <img alt="imdb" src="img/videoland-logo.png" width="100" height="100">
+        <img alt="imdb" src="../img/videoland-logo.png" width="100" height="100">
         </a>
 
         <a href="https://www.movies.nl" target="_blank">
-        <img alt="imdb" src="img/movies-logo.png" width="100" height="100">
+        <img alt="imdb" src="../img/movies-logo.png" width="100" height="100">
         </a>
     </div> 
     
