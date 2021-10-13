c7d6c05e04e33e43babe2a9ca048a9eb0b79b048
BoazWelsenis <D281029@edu.rocwb.nl>
2021-10-13 13:50:23 +0200

    Wijzigingsverzoek klant (header / bestanden)
    De header + het toevoegen / aanpassen van bestanden is doorgevoerd vanuit Wijzigingsverzoek.docx

--- header.php
+++ header.php
@@ -18,19 +18,30 @@
         </div> 
 
         <div class="dropdown">
-                <button class="dropbtn">Informatie 
+                <button class="dropbtn">Gereedschappen 
                     <i class="fa fa-caret-down"></i>
                 </button>
             <div class="dropdown-content">
-                <a href="<?php echo $base_url ?>/information/boeken.php">Boeken</a>
-                <a href="<?php echo $base_url ?>/information/cirkelzagen.php">Cirkelzagen</a>
-                <a href="<?php echo $base_url ?>/information/kettingzagen.php">Kettingzagen</a>
-                <a href="<?php echo $base_url ?>/information/hamers.php">Hamers</a>
-                <a href="<?php echo $base_url ?>/information/jamesbond.php">James Bond</a>
+                <a href="<?php echo $base_url ?>/tools/boeken.php">Boeken</a>
+                <a href="<?php echo $base_url ?>/tools/cirkelzagen.php">Cirkelzagen</a>
+                <a href="<?php echo $base_url ?>/tools/kettingzagen.php">Kettingzagen</a>
+                <a href="<?php echo $base_url ?>/tools/hamers.php">Hamers</a>
+                <a href="<?php echo $base_url ?>/tools/handzagen.php">Handzagen</a>
+                <a href="<?php echo $base_url ?>/tools/schroevendraaiers.php">Schroevendraaiers</a>
+            </div>
+        </div>
+
+        
+        <div class="dropdown">
+            <button class="dropbtn">Informatie 
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+            <a href="<?php echo $base_url ?>/information/jamesbond.php">James Bond</a>
                 <a href="<?php echo $base_url ?>/information/darthvader.php">Darth Vader</a>
                 <a href="<?php echo $base_url ?>/information/timontydalton.php">Timonty Dalton</a>
                 <a href="<?php echo $base_url ?>/information/lukeskywalker.php">Luke Skywalker</a>
             </div>
-        </div>
+        </div> 
     </div>
 </header>
