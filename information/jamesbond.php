4d76a7ec4919b31f70c2372fb139dfa215e656d6
rickbruijs <D283966@edu.rocwb.nl>
2021-10-15 14:28:53 +0200

    lijst van james bond films met roger moore gemaakt

--- information/jamesbond.php
+++ information/jamesbond.php
@@ -13,10 +13,19 @@
     ?>
 
     <main>
-        <div class="wrapper">
-        <a href="jamesbond.php">C2PRA/information/jamesbond.php</a>
-            <h1>James Bond</h1>
-            
+        <div class="wrapper">      
+        <h1>Lijst van James Bond films met Roger Moore</h1>
+            <ul>
+                <div class="grid">
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0070328/">Live and Let Die</a><a href="https://www.imdb.com/title/tt0070328"><img src="../img/James-bond-covers/live-and-let-die.jpg" alt="Cover"></a></li></div>
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0071807/?ref_=nv_sr_srsg_0">The Man with the Golden Gun</a><a href="https://www.imdb.com/title/tt0071807/?ref_=nv_sr_srsg_0"><img src="../img/James-bond-covers/the-man-with-the-golden-gun.jpg" alt="Cover"></a></li></div>
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0076752/?ref_=fn_al_tt_1">The Spy Who Loved Me</a><a href="https://www.imdb.com/title/tt0076752/?ref_=fn_al_tt_1"><img src="../img/James-bond-covers/the-spy-who-loved-me.jpg" alt="Cover"></a></li></div>
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0079574/?ref_=fn_al_tt_1">Moonraker</a><a href="https://www.imdb.com/title/tt0079574/?ref_=fn_al_tt_"><img src="../img/James-bond-covers/moonraker.jpg" alt="Cover"></a></li></div>
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0082398/">For Your Eyes Only</a><a href="https://www.imdb.com/title/tt0082398/"><img src="../img/James-bond-covers/for-your-eyes-only.jpg" alt="Cover"></a></li></div>
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0086034/?ref_=nv_sr_srsg_0">Octopussy<a href="https://www.imdb.com/title/tt0086034/?ref_=nv_sr_srsg_0"><img src="../img/James-bond-covers/octopussy.jpg" alt="Cover"></a></li></div>
+                    <div class="film-item"><li><a href="https://www.imdb.com/title/tt0090264/?ref_=fn_al_tt_1">A View to a Kill<a href="https://www.imdb.com/title/tt0090264/?ref_=fn_al_tt_1"><img src="../img/James-bond-covers/a-view-to-kill.jpg" alt="Cover"></a></li></div>
+                </div>
+            </ul>
         </div>
     </main>
 </body>
