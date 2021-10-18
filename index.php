b6c59554050a55369e82163d06405b68f0d51012
Tijn Van de Velde <vandeveldetijn@gmail.com>
2021-10-18 12:23:47 +0200

    naam geving <title> tag aangepast
    mooier afgerond/spel fouten aangespast

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Home</title>
+    <title>C2 CodeCamp - Home</title>
 </head>
 <body>    
     <?php include_once('header.php'); ?>
