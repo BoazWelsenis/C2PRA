d40dc017e1c9494aaf282e9101518062dc93df76
BoazWelsenis <D281029@edu.rocwb.nl>
2021-10-20 13:35:41 +0200

    Resource toegevoegd Iframe (Google Maps)

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
                     <a class="mail-link" href="mailto:john.doe@hotmail.com">john.doe@hotmail.com</a>
                     <p class="adres">Koestraat 1 Den Bosch</p>
                     
+                    <!-- https://www.mapsdirections.info/nl/maak-een-google-map/ -->
                     <div class="plattegrond-omgeving">
                         <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=nl&amp;q=Koestraat%201%20Den%20Bosch+(Koestraat%201%20Den%20Bosch)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.mapsdirections.info/nl/cirkel-straal-kaart/">Cirkel op kaart Google</a></iframe></div>
                     </div>
