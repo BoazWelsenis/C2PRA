3ae65c431376982125adcc4a28f684f53387d015
BoazWelsenis <D281029@edu.rocwb.nl>
2021-11-01 14:33:17 +0100

    Like Systeem Songteksten (blue & green)
    Het like systeem voor songteksten is gemaakt. De dingen die niet werken zoals het hoort staan vermeld op het planbord bij de ticket in de beschrijving.
    
    Het like systeem voor de andere pagina's is in de vorige commit door Jano - @Z1XCY toegevoegd.

--- music/bluemusic.php
+++ music/bluemusic.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/likesystem.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>C2 CodeCamp - Blauw</title>
 </head>
@@ -25,7 +27,12 @@
                         fclose($myfile);
                     ?>
                 </div>
-                <i onclick="myFunction(this)" class="fa fa-thumbs-up" style="font-size: 40px;"></i>
+                <div class="heart-btn">
+                    <div class="content">
+                    <span class="text"><!-- Hier moet een echo komen en dynamisch worden gemaakt met de database -->Like</span>
+                    <span class="numb"></span>
+                    </div>
+                </div>
             </div>
             
             <div class="queen">
@@ -37,7 +44,12 @@
                         fclose($myfile);
                     ?>
                 </div>
-                <i onclick="myFunction(this)" class="fa fa-thumbs-up" style="font-size: 40px;"></i>
+                <div class="heart-btn">
+                    <div class="content">
+                    <span class="text"><!-- Hier moet een echo komen en dynamisch worden gemaakt met de database -->Like</span>
+                    <span class="numb"></span>
+                    </div>
+                </div>
             </div>
 
             <div class="queen">
@@ -50,15 +62,25 @@
                         fclose($myfile);
                     ?>
                 </div>
-                <i onclick="myFunction(this)" class="fa fa-thumbs-up" style="font-size: 40px;"></i>
+                <div class="heart-btn">
+                    <div class="content">
+                    <span class="text"><!-- Hier moet een echo komen en dynamisch worden gemaakt met de database -->Like</span>
+                    <span class="numb"></span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
     <script>
-    function myFunction(x) {
-  x.classList.toggle("fa-thumbs-down");
-}
-</script>
+        $(document).ready(function(){
+        $('.content').click(function(){
+            $('.content').toggleClass("heart-active")
+            $('.text').toggleClass("heart-active")
+            $('.numb').toggleClass("heart-active")
+        });
+        });
+    </script>
     
 </body>
 </html>
