3b67b907e52f71198ae7249e3849a62936028e89
BoazWelsenis <D281029@edu.rocwb.nl>
2021-11-03 13:54:11 +0100

    Admin pagina (basic styling + beveiliging)

--- admin.php
+++ admin.php
@@ -1,2 +1,30 @@
 <?php session_start(); ?>
-<h1>BOAZ IS LELIJK</h1>
+
+<?php
+require_once 'backend/config.php';
+if(!isset($_SESSION['user_id']))
+{
+	header("Location: $base_url/login.php");
+	exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Admin</title>
+</head>
+<body>
+    <?php include_once('header.php'); ?>
+
+    <div class="wrapper">
+        <h1 class="title-page">Admin</h1>
+        
+        <!-- HIER MOET VERDERE CONTENT KOMEN TE STAAN -->
+    </div>
+</body>
+</html>
