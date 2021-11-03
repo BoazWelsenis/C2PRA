3c9614ad6abc6b7bee561226055e55358c0d12b6
BoazWelsenis <D281029@edu.rocwb.nl>
2021-11-03 20:35:40 +0100

    Opslaan decimals (met 2 decimalen) werkt & title add-book.php toegevoegd
    De title voor de add-book.php pagina is toegevoegd.
    
    En het is nu ook mogelijk om kommagetallen op te slaan in de database (bij de prijs, als decimal).

--- add-book.php
+++ add-book.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Boek toevoegen</title>
 </head>
 <body>
     <?php include_once('header.php') ?>
@@ -30,7 +30,7 @@
         </div>
         <div class="form-group">
             <label for="prijs">Prijs</label>
-            <input type="number" name="prijs" id="prijs">
+            <input type="number" step="any" name="prijs" id="prijs"> <!-- Save decimals in MySQL Table: https://stackoverflow.com/questions/16629759/how-to-store-decimal-in-mysql -->
         </div>
         <div class="form-group">
             <label for="buylink">Link naar product</label>
