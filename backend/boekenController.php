0679efb9cf5887dc55cad8d374d0de216552731c
BoazWelsenis <D281029@edu.rocwb.nl>
2021-11-03 15:08:57 +0100

    Controller voor Boeken & Songteksten  pagina af
    De Controllers zorgen ervoor dat de items uit het ingevulde form in de database terecht komen.

--- backend/boekenController.php
+++ backend/boekenController.php
@@ -1,3 +1,32 @@
 <?php
-    
+    session_start();
+    require_once 'config.php';
+
+    if(!isset($_SESSION['user_id']))
+    {
+        header("Location: $base_url/login.php");
+        exit;
+    }
+
+    $titel = $_POST['titel'];
+    $auteur = $_POST['auteur'];
+    $bladzijdes = $_POST['bladzijdes'];
+    $beschrijving = $_POST['beschrijving'];
+    $prijs = $_POST['prijs'];
+    $buylink = $_POST['buylink'];
+
+    require_once 'conn.php';
+    $query = "INSERT INTO boeken (titel, auteur, bladzijdes, beschrijving, prijs, buylink) VALUES(:titel, :auteur, :bladzijdes, :beschrijving, :prijs, :buylink)";
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":titel" => $titel,
+        ":auteur" => $auteur,
+        ":bladzijdes" => $bladzijdes,
+        ":beschrijving" => $beschrijving,
+        ":prijs" => $prijs,
+        ":buylink" => $buylink
+    ]);
+
+    header("Location: $base_url/books/boeken.php");
+    exit;
 ?>
--- backend/songtekstenController.php
+++ backend/songtekstenController.php
@@ -1,3 +1,26 @@
 <?php
-    
+    session_start();
+    require_once '/config.php';
+
+    if(!isset($_SESSION['user_id']))
+    {
+        header("Location: $base_url/login.php");
+        exit;
+    }
+
+    $titel = $_POST['titel'];
+    $artiest = $_POST['artiest'];
+    $songtekst = $_POST['songtekst'];
+
+    require_once 'conn.php';
+    $query = "INSERT INTO songteksten (titel, artiest, songtekst) VALUES(:titel, :auteur, :songtekst)";
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":titel" => $titel,
+        ":artiest" => $artiest,
+        ":songtekst" => $songtekst
+    ]);
+
+    header("Location: $base_url/books/boeken.php");
+    exit;
 ?>
