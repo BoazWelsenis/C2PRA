d5d3ff01076dfa65c85233d7ced792faedef52a5
Tijn Van de Velde <vandeveldetijn@gmail.com>
2021-11-10 13:54:48 +0100

    update logincontroller.php
    naam veranderd +user

--- backend/loginController.php
+++ backend/loginController.php
@@ -23,6 +23,6 @@ if(!password_verify($password, $user['password']))
 }
 
 $_SESSION['user_id'] = $user['id'];
-$_SESSION['user_name'] = $user['name'];
+$_SESSION['user_name'] = $user['username'];
 header("Location: $base_url/admin.php");
 ?>
