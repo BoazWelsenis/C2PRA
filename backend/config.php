b5960f97317e665dd2f1f1f97897f4bc9b1fa6bf
BoazWelsenis <D281029@edu.rocwb.nl>
2021-11-10 13:28:12 +0100

    .gitignore is working

--- backend/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbHost = 'localhost';
-$dbName = 'C2PRA';
-$dbUser = 'root';
-$dbPass = '';
-
-//De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'http://localhost/C2PRA';
