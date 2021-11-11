e45e57b848a4084352f12769b61f0dfc6e025eca
BoazWelsenis <D281029@edu.rocwb.nl>
2021-11-11 18:05:19 +0100

    Beveiliging Add & Edit pagina's
    Beveiliging is toegevoegd bij de Add & Edit pagina's van boeken en songteksten.
    
    Dit betekent dat je de link niet kan bezoeken wanneer je niet bent ingelogd. Dit is dus precies wat je wilt, aangezien alleen de admin content hoort aan te passen of toe te voegen.

--- edit-songtekst.php
+++ edit-songtekst.php
@@ -1,3 +1,14 @@
+<?php session_start(); ?>
+
+<?php
+require_once 'backend/config.php';
+if(!isset($_SESSION['user_id']))
+{
+	header("Location: $base_url/login.php");
+	exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
